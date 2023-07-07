7e8c8b47a500b658ed77fb431932e2720ab0c457
Sp3lllz <136860095+sp3lllz@users.noreply.github.com>
2023-07-07 23:22:19 +0100

    added install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Get the logged-in username
+username=$(whoami)
+
+echo "Installing Pimoroni Inky library..."
+# Install Pimoroni Inky library
+curl https://get.pimoroni.com/inky | bash
+
+echo "Creating 'images' folder in the user's home directory..."
+# Create the "images" folder in the user's home directory if it doesn't exist
+mkdir -p /home/"$username"/images
+
+echo "Downloading 'ranphoto.py' script..."
+# Download the Python script from the URL and save it in the user's home directory
+curl -o /home/"$username"/ranphoto.py https://raw.githubusercontent.com/sp3lllz/inkyphotoframe/main/ranphoto.py
+
+echo "Making 'ranphoto.py' script executable..."
+# Make the downloaded Python script executable
+chmod +x /home/"$username"/ranphoto.py
+
+echo "Copying 'image.py' file to the user's home directory..."
+# Copy the image.py file to the user's home directory
+cp /home/"$username"/Pimoroni/inky/examples/7color/image.py /home/"$username"
+
+echo "Adding cron job to the crontab..."
+# Add cron job to the crontab
+(crontab -l -u "$username" 2>/dev/null; echo "*/30 * * * * python3 /home/$username/ranphoto.py") | crontab -u "$username" -
+
+echo "Cron job added successfully."
