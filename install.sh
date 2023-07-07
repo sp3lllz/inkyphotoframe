a527dc55d564e6ee87ee9a8b7a25907930e20570
Sp3lllz <136860095+sp3lllz@users.noreply.github.com>
2023-07-07 23:28:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ curl https://get.pimoroni.com/inky | bash
 
 echo "Creating 'images' folder in the user's home directory..."
 # Create the "images" folder in the user's home directory if it doesn't exist
-mkdir -p /home/"$username"/images
+sudo mkdir -p /home/"$username"/images
 
 echo "Downloading 'ranphoto.py' script..."
 # Download the Python script from the URL and save it in the user's home directory
@@ -17,11 +17,11 @@ curl -o /home/"$username"/ranphoto.py https://raw.githubusercontent.com/sp3lllz/
 
 echo "Making 'ranphoto.py' script executable..."
 # Make the downloaded Python script executable
-chmod +x /home/"$username"/ranphoto.py
+sudo chmod +x /home/"$username"/ranphoto.py
 
 echo "Copying 'image.py' file to the user's home directory..."
 # Copy the image.py file to the user's home directory
-cp /home/"$username"/Pimoroni/inky/examples/7color/image.py /home/"$username"
+sudo cp /home/"$username"/Pimoroni/inky/examples/7color/image.py /home/"$username"
 
 echo "Adding cron job to the crontab..."
 # Add cron job to the crontab
