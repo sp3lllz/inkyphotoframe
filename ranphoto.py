08c00d1dca72c9b69b5e42bb41ac11bcf455b6c6
Sp3lllz <136860095+sp3lllz@users.noreply.github.com>
2023-07-07 22:58:25 +0100

    Added ranphoto.py

---
+++ ranphoto.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import random
+import os
+import signal
+import datetime
+import requests
+import subprocess
+import RPi.GPIO as GPIO
+import sys
+from PIL import Image
+from inky.auto import auto
+# Generate the random numer
+randnum = str(random.randint(1,77))
+# print the number generated
+print("The Number is: {}".format(randnum))
+# set the variable for the photo path
+image_file = "images/main/{}.jpg".format(randnum)
+# run the image.py script to display the chosen image on the screen
+os.system("python image.py " + image_file)
