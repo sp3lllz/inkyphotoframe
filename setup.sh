a46381d8aa8b4824b63181aa8c0e08f9bb2b64f0
Sp3lllz <136860095+sp3lllz@users.noreply.github.com>
2023-07-08 23:10:21 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# Download ranphoto.py
-curl -o /home/spell/ranphoto.py https://raw.githubusercontent.com/sp3lllz/inkyphotoframe/main/ranphoto.py
+# Clone the repository
+git clone https://github.com/sp3lllz/inkyphotoframe.git /tmp/inkyphotoframe
+# Move the required file
+mv /tmp/inkyphotoframe/ranphoto.py /home/spell/ranphoto.py
 # Set permissions for ranphoto.py
 chmod +x /home/spell/ranphoto.py
 # Copy image.py
 cp /home/spell/Pimoroni/inky/examples/7color/image.py /home/spell/image.py
+# Cleanup
+rm -rf /tmp/inkyphotoframe
