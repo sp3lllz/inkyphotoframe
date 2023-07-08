3315e015337070407aead78efbae52b13480efc9
Sp3lllz <136860095+sp3lllz@users.noreply.github.com>
2023-07-08 22:52:48 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-curl https://get.pimoroni.com/inky | bash
 curl -o /home/spell/ranphoto.py https://raw.githubusercontent.com/sp3lllz/inkyphotoframe/main/ranphoto.py 
 cp /home/spell/Pimoroni/inky/examples/7color/image.py /home/spell/image.py
 chmod +x ranphoto.py
