0bc03a24e3f85dc55158e43a3bcd7b9b17a6f616
Sp3lllz <136860095+sp3lllz@users.noreply.github.com>
2024-11-20 20:07:53 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,24 +6,24 @@ echo "This script will make changes to your device including installing software
 #install pimoroni software
 curl https://get.pimoroni.com/inky | bash
 #create nessisary folders
-sudo mkdir /home/$USER/inkyframe
-sudo mkdir /home/$USER/inkyframe/images
-sudo mkdir /home/$USER/inkyframe/images/main
-sudo chown -R $USER:$USER /home/$USER/inkyframe
-sudo chmod -R u+rwx /home/$USER/inkyframe
+sudo mkdir /var/inkyframe
+sudo mkdir /var/inkyframe/images
+sudo mkdir /var/inkyframe/images/main
+sudo chown -R $USER:$USER /var/inkyframe
+sudo chmod -R u+rwx /var/inkyframe
 #download scipts from github
-curl -o /home/$USER/inkyframe/ranphoto.py https://raw.githubusercontent.com/sp3lllz/inkyphotoframe/main/ranphoto.py
-curl -o /home/$USER/inkyframe/ranphoto.sh https://raw.githubusercontent.com/sp3lllz/inkyphotoframe/refs/heads/main/randphoto.sh
-curl -o /home/$USER/inkyframe/ranupdate.sh https://raw.githubusercontent.com/sp3lllz/inkyphotoframe/refs/heads/main/ranupdate.sh
+curl -o /var/inkyframe/ranphoto.py https://raw.githubusercontent.com/sp3lllz/inkyphotoframe/main/ranphoto.py
+curl -o /var/inkyframe/ranphoto.sh https://raw.githubusercontent.com/sp3lllz/inkyphotoframe/refs/heads/main/randphoto.sh
+curl -o /var/inkyframe/ranupdate.sh https://raw.githubusercontent.com/sp3lllz/inkyphotoframe/refs/heads/main/ranupdate.sh
 #copy image.py script from the pimironi folder to the inkyframe folder
-cp /home/$USER/Pimoroni/inky/examples/7color/image.py /home/$USER/inkyframe/image.py
+cp /var/Pimoroni/inky/examples/7color/image.py /var/inkyframe/image.py
 #make scripts executable
-sudo chmod +x /home/$USER/inkyframe/ranphoto.py
-sudo chmod +x /home/$USER/inkyframe/ranphoto.sh
-sudo chmod +x /home/$USER/inkyframe/image.py
-sudo chmod +x /home/$USER/inkyframe/ranupdate.sh
+sudo chmod +x /var/inkyframe/ranphoto.py
+sudo chmod +x /var/inkyframe/ranphoto.sh
+sudo chmod +x /var/inkyframe/image.py
+sudo chmod +x /var/inkyframe/ranupdate.sh
 #setup chronjob
-inkyjob="*/30 * * * * bash /home/$USER/inkyframe/ranphoto.sh"
+inkyjob="*/30 * * * * bash /var/inkyframe/ranphoto.sh"
 if crontab -l | grep -Fxq "$inkyjob"; then
     echo "The cron job is already scheduled."
 else
@@ -33,8 +33,8 @@ fi
 echo "Installation completed"
 echo "We will now display a test image to test everything is working."
 #download test image
-curl -o /home/$USER/inkyframe/testimage.jpg https://raw.githubusercontent.com/sp3lllz/inkyphotoframe/ebaf87d4ec4db4c45492eafdbb4082691fef1315/inkytest.jpg
+curl -o /var/inkyframe/testimage.jpg https://raw.githubusercontent.com/sp3lllz/inkyphotoframe/ebaf87d4ec4db4c45492eafdbb4082691fef1315/inkytest.jpg
 source ~/.virtualenvs/pimoroni/bin/activate
-python3 /home/$USER/inkyframe/image.py --file /home/$USER/inkyframe/testimage.jpg
-echo "please place images in the /home/$USER/inkyframe/images/main folder formatted as per the Readme on https://github.com/sp3lllz/inkyphotoframe/blob/main/README.md"
+python3 /var/inkyframe/image.py --file /var/inkyframe/testimage.jpg
+echo "please place images in the /var/inkyframe/images/main folder formatted as per the Readme on https://github.com/sp3lllz/inkyphotoframe/blob/main/README.md"
 echo "after images have been placed as per the readme please update the maximum number for the random number generator using ranupdate.sh please see the readme for more details"
